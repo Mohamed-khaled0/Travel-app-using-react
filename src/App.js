f90fbf19f552469168ee2115e33ce46e911f24ad
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-10-18 14:52:42 +0300

    Create search section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Destinations from "./components/Destinations";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-<Destinations/>
+      <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
     </div>
   );
 }
